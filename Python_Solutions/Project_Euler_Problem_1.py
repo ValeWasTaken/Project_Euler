856eb651486f7055166dde196337b516dffbf25d
Michael Arnold <valestrum@hotmail.com>
2014-02-23 01:00:41 -0500

    Removed useless comments.

--- Python_Solutions/Project_Euler_Problem_1.py
+++ Python_Solutions/Project_Euler_Problem_1.py
@@ -4,7 +4,7 @@
 def main():
     answer = 0
     for x in range(1000): # Uses X to check numbers up to 1000.
-        if (x % 5 == 0 or x % 3 == 0): # If X is divisable by 3 or 5 execute code below
-            answer = answer + x #Add number to the total sum being calculated
-    print answer #Display total sum of all multiples of 3 or 5 below 1000 to the user.
+        if (x % 5 == 0 or x % 3 == 0):
+            answer = answer + x
+    print answer #Prints sum of all multiples of 3 or 5 below 1000.
 main()
