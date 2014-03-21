4013318c5f0e05c697544bba7978cb0b3d3ca184
Michael Arnold <valestrum@hotmail.com>
2014-03-21 09:39:36 -0400

    Greatly lowered procressing power required.

--- Python_Solutions/Project_Euler_Problem_005_Smallest_Multiple.py
+++ Python_Solutions/Project_Euler_Problem_005_Smallest_Multiple.py
@@ -1,7 +1,7 @@
 def main():
-    x = 1
-    while(x != 0):
-        x = x + 1
+    x = 0
+    while(x != -1):
+        x = x + 20
         if all(x % k == 0 for k in range(1, 21)):
             print(x)
             return 0
