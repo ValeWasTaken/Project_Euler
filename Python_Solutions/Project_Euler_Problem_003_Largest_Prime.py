830ca8f67bbe5d17114eff9587cb6c977232cebe
Michael Arnold <valestrum@hotmail.com>
2014-03-27 12:49:57 -0400

    Slightly optimized problem 3.

--- Python_Solutions/Project_Euler_Problem_003_Largest_Prime.py
+++ Python_Solutions/Project_Euler_Problem_003_Largest_Prime.py
@@ -3,7 +3,7 @@ def main():
     x = 2
     while x * x < num: 
         while num % x == 0: 
-            num = num / x #Divide number by generated number (X) to get the prime number.
-        x = x + 1 # Continue in formula searching for largest prime
+            num /= x #Divide number by generated number (X) to get the prime number.
+        x += 1 # Continue in formula searching for largest prime
     print num #Prints largest prime of the assigned number (600851475143)
 main()
