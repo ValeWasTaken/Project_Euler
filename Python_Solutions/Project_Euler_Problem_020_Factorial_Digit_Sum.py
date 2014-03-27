e932d14c7ba7be74e8e52d99e39b307aec605f72
Michael Arnold <valestrum@hotmail.com>
2014-03-27 13:00:21 -0400

    Slightly optimized Problem 20.

--- Python_Solutions/Project_Euler_Problem_020_Factorial_Digit_Sum.py
+++ Python_Solutions/Project_Euler_Problem_020_Factorial_Digit_Sum.py
@@ -8,13 +8,14 @@ def main():
     sum_of_number = 1  #This is the sum of 100! (Not the digits added together)
     
     while(count != 1):
-        count = count - 1
-        sum_of_number = sum_of_number * count
+        count -= 1
+        sum_of_number *= count
 
     sum_of_number = str(sum_of_number)
 
     for digit in sum_of_number:
-        sum_of_digits = sum_of_digits + int(digit)
+        sum_of_digits += int(digit)
 
     print(sum_of_digits)
 main()
+
