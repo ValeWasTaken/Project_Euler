e4ed244250698f7c60dedacb322033cddb16cbc5
Michael Arnold <valestrum@hotmail.com>
2014-03-27 12:50:36 -0400

    Slightly optimized Problem 1.

--- Python_Solutions/Project_Euler_Problem_001.py
+++ Python_Solutions/Project_Euler_Problem_001.py
@@ -5,6 +5,6 @@ def main():
     answer = 0
     for x in range(1000): # Uses X to check numbers up to 1000.
         if (x % 5 == 0 or x % 3 == 0):
-            answer = answer + x
+            answer += x
     print answer #Prints sum of all multiples of 3 or 5 below 1000.
 main()
