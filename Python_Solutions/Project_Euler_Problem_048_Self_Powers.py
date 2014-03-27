98264e0da9e5c1d013b99e342abe11cd0715ecc7
Michael Arnold <valestrum@hotmail.com>
2014-03-27 12:40:57 -0400

    Optimized problem 44

--- Python_Solutions/Project_Euler_Problem_048_Self_Powers.py
+++ Python_Solutions/Project_Euler_Problem_048_Self_Powers.py
@@ -2,13 +2,12 @@
 # Find the 10 digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
 def main():
-    count = 1
-    temp_num = 0
     total = 0
-    while (count != 1000):
-        temp_num = count**count
-        total = total + temp_num
-        count = count + 1
-    total = str(total)
-    print total[-10:]
+    for x in range(1, 1001):
+        total += x**x
+    print str(total)[-10:] #Prints the last 10 digits of the total.
 main()
+
+#Note: This solution could be one line by using the following below code.
+#However, I did not come up with it, nor do I like it as much as my solution due to readability reasons.
+#print(str(sum([x**x for x in range(1,1001)]))[-10:])
