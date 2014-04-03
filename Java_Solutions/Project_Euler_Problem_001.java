79ad7952d4b72868cf2d2a7e1e9ea61c486e3fe2
Michael Arnold <valestrum@hotmail.com>
2014-04-03 10:14:27 -0400

    Created the solution to Problem 1 in Java.

---
+++ Java_Solutions/Project_Euler_Problem_001.java
@@ -0,0 +1,13 @@
+public class Project_Euler_Problem_1 {
+	public static void main(String args[]){
+		int answer = 0;
+		for (int x=0; x<1000; x++)
+		{
+			if (x % 5 == 0 || x % 3 == 0)
+			{
+				answer += x;
+			}
+		}
+		System.out.println("The sum of all multiples of 3 and 5 is " + answer + ".\n\n"); // answer should be 233168.
+	}
+}
