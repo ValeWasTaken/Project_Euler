f073544d1615efa759d81f22c68d0258edf4f1a5
Michael Arnold <valestrum@hotmail.com>
2014-04-06 10:46:38 -0400

    Created the solution to Problem 6 in Java.

---
+++ Java_Solutions/Project_Euler_Problem_006.java
@@ -0,0 +1,26 @@
+public class Project_Euler_Problem_006
+{
+	public static void main(String args[])
+	{
+		// Start of "Square of sums" block
+		int n = 0;
+		for (int x = 0; x < 101; x++)
+		{
+			n = n + x;
+		}
+		int final_num = n*n;
+		System.out.println("The square of the sum of the first 100 natural numbers is: " + final_num); // Expected output: 25502500
+		
+		// Start of "Sum of squares" block
+		int num = 0;
+		for (int y = 0; y < 101; y++)
+		{
+			num = num + (y*y);
+		}
+		System.out.println("The sum of the squares of the first 100 natural numbers is: " + num); // Expected output: 338350
+		
+		// Final calculation
+		int difference = final_num - num;
+		System.out.println("The difference between the two sums is: " + difference); // Expected output: 25164150
+	}
+}
