6306c14240ca44b23ebf98ecc9f7d5b995e93ee1
Michael Arnold <valestrum@hotmail.com>
2014-04-09 12:05:55 -0400

    Created solution to Problem 2 in PHP.

---
+++ PHP_Solutions/Project_Euler_Problem_002.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = 0; $b = 1; $c = 0; $d = 0;
+while ($c < 3500000)
+{
+	$c = $a + $b;
+	$a = $b;
+	$b = $c;
+	if ($c % 2 == 0)
+	{
+		$d += $c;
+	}
+}
+
+echo "The sum of all EVEN fibonacci values under 4 million is: " + $d;
+// Expected answer: 4613732
+?>
