2e6a2938a5c2374b65a34ec65cedb7cf9e64c323
Michael Arnold <valestrum@hotmail.com>
2014-04-12 23:39:15 -0400

    Slightly optimized PHP solution to Problem 1.

--- PHP_Solutions/Project_Euler_Problem_001.php
+++ PHP_Solutions/Project_Euler_Problem_001.php
@@ -5,7 +5,7 @@ for ($x = 0; $x<1000; $x++)
 {
 	if ($x % 5 == 0 || $x % 3 == 0)
 	{
-		$answer = $answer + $x;
+		$answer += $x;
 	}
 }
 echo "The sum of all multiples of 3 and 5 is $answer."; // The answer should be 233168.
