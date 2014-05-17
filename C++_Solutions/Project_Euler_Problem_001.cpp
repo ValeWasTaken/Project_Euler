783111d0af7dbf5edb0ce8d2bc181d47315a7886
Michael Arnold <valestrum@hotmail.com>
2014-05-17 00:39:56 -0400

    Deleted useless comments

--- C++_Solutions/Project_Euler_Problem_001.cpp
+++ C++_Solutions/Project_Euler_Problem_001.cpp
@@ -8,9 +8,9 @@ int main()
 	int answer = 0;
 	for (int x=0; x<1000; x++) // Uses X to check numbers up to 1000.
 	{ 
-		if (x % 5 == 0 || x % 3 == 0) // If X if divisable by 5 or 3 execute below code
+		if (x % 5 == 0 || x % 3 == 0) 
 		{ 
-			answer += x; // Add number to total sum (Also written as "answer = answer + x;")
+			answer += x; 
 		}
 	}
 	std::cout << "The sum of all multiples of 3 and 5 is " << answer << ".\n\n";
