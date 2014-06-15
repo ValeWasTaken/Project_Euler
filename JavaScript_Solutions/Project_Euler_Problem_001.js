4466a51f7d3ccd71dcb39f26cca0329f0afd2a6b
Michael Arnold <valestrum@hotmail.com>
2014-06-14 20:36:19 -0400

    Created solution to Project Euler Problem #1 in JS

---
+++ JavaScript_Solutions/Project_Euler_Problem_001.js
@@ -0,0 +1,9 @@
+var answer = 0;
+for (var x=0; x<1000; x++)
+{
+        if(x % 5 === 0 || x % 3 === 0)
+        {
+                answer += x;
+        }
+}
+alert("The sum of all multiples of 3 or 5 below 1000 is: " + answer); // The answer should be: 233168
