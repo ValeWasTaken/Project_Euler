5064949edd087a14f218d221b9803316146a1702
Michael Arnold <valestrum@hotmail.com>
2014-07-30 23:20:56 -0400

    Removed useless comments and condensed.

--- C++_Solutions/Project_Euler_Problem_002.cpp
+++ C++_Solutions/Project_Euler_Problem_002.cpp
@@ -2,17 +2,15 @@
 
 int main()
 {
-	int a = 0, b = 1, c = 0, d = 0; // Defines variables for the rest of the program
-	while (c < 3500000) // While value "c" is less than 3.5mil, execute below code.
+	int a = 0, b = 1, c = 0, d = 0;
+	while (c < 3500000)
 	{
 		c = a + b; // Calculates fibonacci sequence starting at 0,1,1,..
-		std::cout << c << std::endl; // Displays current number in sequence being counted.
-		a = b; // Continues Fibonacci sequence
+		std::cout << c << std::endl;
+		a = b; 
 		b = c;
 		if (c % 2 == 0)
-		{
-			d += c;
-		}
+		{d += c;}
 	}
 	std::cout << "The sum of all EVEN fibonacci values under 4million is: " << d << std::endl;
 	return 0;
