ab6bb14f7b5f200c68b136c3cdfe8e927c8530af
Michael Arnold <valestrum@hotmail.com>
2014-07-30 23:20:11 -0400

    Removed useless comment and condensed.

--- C++_Solutions/Project_Euler_Problem_003.cpp
+++ C++_Solutions/Project_Euler_Problem_003.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-    // Optionally, you could replace my "std::"s with "using namespace std;" here.
+
 int main()
 {
 	long long int num = 600851475143; // Replace this with any largest prime number you want.
@@ -7,9 +7,7 @@ int main()
 	while (x*x < num)
 	{
 		while (num % x == 0)
-		{
-			num /= x; 
-		}
+		{num /= x;}
 		x = x++;
 	}
 	std::cout << num;
