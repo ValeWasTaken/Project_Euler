6c86abf2711f6afd81b1a9ce20f5a1de56850fb9
Michael Arnold <valestrum@hotmail.com>
2014-07-30 23:05:29 -0400

    Condensed program

--- C++_Solutions/Project_Euler_Problem_006.cpp
+++ C++_Solutions/Project_Euler_Problem_006.cpp
@@ -5,18 +5,14 @@ int main()
 	// Start of "Square of sums" block
 	int n = 0;
 	for (int x = 0; x < 101; x++)
-	{
-		n += x;
-	}
+	{n += x;}
 	int final_num = n*n;
 	std::cout << "The square of the sum of the first 100 natural numbers is: " << final_num << std::endl; // Expected output: 25502500
 
 	// Start of "Sum of squares" block
 	int num = 0;
 	for (int y = 0; y < 101; y++)
-	{
-		num += (y*y);
-	}
+	{num += (y*y);}
 	std::cout << "The sum of the squares of the first 100 natural numbers is: " << num << std::endl; // Expected output: 338350
 
 	// Final calculation
