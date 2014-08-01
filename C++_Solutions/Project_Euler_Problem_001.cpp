8ef0ee752c17abf149780c25c3cae9f5656ae3e3
Michael Arnold <valestrum@hotmail.com>
2014-08-01 10:16:37 -0400

    Condensed program

--- C++_Solutions/Project_Euler_Problem_001.cpp
+++ C++_Solutions/Project_Euler_Problem_001.cpp
@@ -7,9 +7,7 @@ int main()
 	for (int x=0; x<1000; x++) // Uses X to check numbers up to 1000.
 	{ 
 		if (x % 5 == 0 || x % 3 == 0) 
-		{ 
-			answer += x; 
-		}
+		{answer += x;}
 	}
 	std::cout << "The sum of all multiples of 3 and 5 is " << answer << ".\n\n";
 	return 0;  
