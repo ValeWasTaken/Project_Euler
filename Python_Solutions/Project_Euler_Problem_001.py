2cde61eb8477b4766230dfbf8415f114f49f1e77
Michael Arnold <valestrum@hotmail.com>
2014-08-12 20:07:02 -0400

    Removed useless comments

--- Python_Solutions/Project_Euler_Problem_001.py
+++ Python_Solutions/Project_Euler_Problem_001.py
@@ -1,6 +1,3 @@
-#Project Euler - Problem 1: Multiples of 3 and 5
-#Goal: Find the sum of all the multiples of 3 or 5 below 1000.
-#Date of completion: 1/2/2014
 def main():
     answer = 0
     for x in range(1000): # Uses X to check numbers up to 1000.
