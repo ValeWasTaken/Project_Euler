6328831f34a8c8e5bc12ab2dc6042d7b5905dc52
Michael Arnold <valestrum@hotmail.com>
2014-08-13 23:21:07 -0400

    Removed useless comments

--- Python_Solutions/Project_Euler_Problem_048_Self_Powers.py
+++ Python_Solutions/Project_Euler_Problem_048_Self_Powers.py
@@ -1,6 +1,3 @@
-# Project Euler Problem 48 - Self Powers
-# Find the 10 digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
-
 def main():
     total = 0
     for x in range(1, 1001):
@@ -9,5 +6,4 @@ def main():
 main()
 
 #Note: This solution could be one line by using the following below code.
-#However, it's basically the same thing but with worse readability in my opinion.
 #print(str(sum([x**x for x in range(1,1001)]))[-10:])
