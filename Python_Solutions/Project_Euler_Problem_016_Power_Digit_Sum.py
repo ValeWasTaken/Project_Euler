20b445567aa4c9b668d9205640c2da644666ba98
Michael Arnold <valestrum@hotmail.com>
2014-08-16 21:19:59 -0400

    Removed useless comments

--- Python_Solutions/Project_Euler_Problem_016_Power_Digit_Sum.py
+++ Python_Solutions/Project_Euler_Problem_016_Power_Digit_Sum.py
@@ -1,8 +1,5 @@
-# Project Euler Problem 16 - Power Digit Sum
-# Find the sum of the digits of 2^1000
-
 def main():
-    original_num = str(2**1000) #2 ^ 1000
+    original_num = str(2**1000)
     sum_of_digits = 0
 
     for digit in original_num:
