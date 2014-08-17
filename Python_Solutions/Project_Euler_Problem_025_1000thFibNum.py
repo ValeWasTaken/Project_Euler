4bec989805dda7d54f76b56b8ca8246b5065ca05
Michael Arnold <valestrum@hotmail.com>
2014-08-16 21:19:08 -0400

    Optimized comments

--- Python_Solutions/Project_Euler_Problem_025_1000thFibNum.py
+++ Python_Solutions/Project_Euler_Problem_025_1000thFibNum.py
@@ -1,13 +1,11 @@
 def main():
     x,y,z = 0,1,0
     counter = 1
-    while len(str(z)) != 1000: #Length counter
-        z = x + y #
-        x = y # Fibonacci sequence
-        y = z #
+    while len(str(z)) != 1000:
+        z = x + y
+        x = y 
+        y = z 
         counter += 1
-    # Add "print z" to display the 1000 digit number.
-    # When checking num it should be: 10700...816
     print("The " + str(counter) + "nd number in the Fibonacci sequence produces the first 1000 digit number.")
-    # counter, the number printed, should be '4782'.
+    # Expected output: 4782
 main()
