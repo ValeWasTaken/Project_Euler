3f5c43f05e7bc29cf5e222001ebd1981582bc831
Michael Arnold <valestrum@hotmail.com>
2014-09-12 23:12:17 -0400

    Removed comments and 4 digit palindrome

--- Python_Solutions/Project_Euler_Problem_004_Largest_palindrome_product.py
+++ Python_Solutions/Project_Euler_Problem_004_Largest_palindrome_product.py
@@ -1,6 +1,3 @@
-#Project Euler - Problem 4
-#Find the largest palindrome made from the product of two 3-digit numbers.
-
 def is_palindrome(number):
     return str(number) == str(number)[::-1]
 
@@ -9,18 +6,3 @@ if __name__ == "__main__":
         for i in range(1000, 900, -1)
             for j in range(1000, 900, -1)
                 if is_palindrome(i*j))))
-
-# The above I learned from watching a tutorial
-# So below I decided to re-do the solution but instead to solve for
-# The largest palindrome made from the product of two 4-digit numbers.
-
-if __name__ == "__main__":
-    print('{} and {} => {}'.format(*max((a,b,a*b)
-        for a in range(10000, 9000, -1)
-            for b in range(10000, 9000, -1)
-                if is_palindrome(a*b))))
-    
-#Note: On line 18 & 19 the number "9000" can be lower, but is set at 9000
-#because it is highly unlikely the number we are searching for will be
-#lower than 9000 and if we include the lower numbers the procressing time
-#and power required would be much higher for no reason.
