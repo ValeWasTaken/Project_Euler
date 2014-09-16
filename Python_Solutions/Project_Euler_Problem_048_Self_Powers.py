994fbf33463ba56e28bc1b9fee321499db28a227
Michael Arnold <valestrum@hotmail.com>
2014-09-16 01:09:40 -0400

    Removed comment & unified naming.

--- Python_Solutions/Project_Euler_Problem_048_Self_Powers.py
+++ Python_Solutions/Project_Euler_Problem_048_Self_Powers.py
@@ -2,8 +2,7 @@ def main():
     total = 0
     for x in range(1, 1001):
         total += x**x
-    print str(total)[-10:] #Prints the last 10 digits of the total.
+    print str(total)[-10:]
 main()
-
 #Note: This solution could be one line by using the following below code.
 #print(str(sum([x**x for x in range(1,1001)]))[-10:])
