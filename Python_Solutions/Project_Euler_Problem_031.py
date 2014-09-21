1c8a40568a70db25435173eb278cfc024daca9a1
Michael Arnold <valestrum@hotmail.com>
2014-09-20 23:09:35 -0400

    Removed useless comment

--- Python_Solutions/Project_Euler_Problem_031.py
+++ Python_Solutions/Project_Euler_Problem_031.py
@@ -1,8 +1,3 @@
-#Project Euler Problem 31 - Coin Sums
-#England currency has eight coins in general circulation:
-# 1p, 2p, 5p, 10p, 20p, 50p, 100p, and 200p
-# How many ways can 200p be made using any number of coins?
-
 def main():
     coins = [1,2,5,10,20,50,100,200]
     goal = 200
