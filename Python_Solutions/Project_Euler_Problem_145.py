9b874b289a5ae1d804857f2467350414916046ba
Michael Arnold <valestrum@hotmail.com>
2015-01-11 14:30:48 -0500

    Update Project_Euler_Problem_145.py

--- Python_Solutions/Project_Euler_Problem_145.py
+++ Python_Solutions/Project_Euler_Problem_145.py
@@ -1,15 +1,16 @@
 def reversible_numbers(limit):
         amount = 0
         for x in range(limit):
-                y = int(str(x)[::-1])
-                if len(str(x)) == len(str(y)):
-                        total = x + y
-                        temp = ''
-                        for digit in str(total):
-                                if int(digit) % 2 != 0:
-                                        temp += str(digit)
-                        if temp == str(total):
-                                amount += 1
+                if x % 2 != 0:
+                        y = int(str(x)[::-1])
+                        if len(str(x)) == len(str(y)):
+                                total = x + y
+                                temp = ''
+                                for digit in str(total):
+                                        if int(digit) % 2 != 0:
+                                                temp += str(digit)
+                                if temp == str(total):
+                                        amount += 1
         print(amount)
 reversible_numbers(input("Find reversible numbers below: "))
 # Unoptimized solution -- Currently still a work-in-progress
