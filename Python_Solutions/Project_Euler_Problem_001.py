0f2abd8207f57bae660c4023f4fa0b38bb402858
Michael Arnold <valestrum@hotmail.com>
2015-05-27 21:12:20 -0400

    Went back and solved with a 1-liner.

--- Python_Solutions/Project_Euler_Problem_001.py
+++ Python_Solutions/Project_Euler_Problem_001.py
@@ -1,7 +1,1 @@
-def main():
-    answer = 0
-    for x in range(1000):
-        if (x % 5 == 0 or x % 3 == 0):
-            answer += x
-    print answer
-main()
+print(sum([x for x in range(1000) if x % 5 == 0 or x % 3 == 0]))
