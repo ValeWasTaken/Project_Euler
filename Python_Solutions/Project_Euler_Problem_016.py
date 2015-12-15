0037f2a694780cc4a57e4091359f89a33ab3874c
Michael Arnold <valestrum@hotmail.com>
2015-12-15 17:53:11 -0500

    Solved problem 16 using a Python 3.4 one-liner

--- Python_Solutions/Project_Euler_Problem_016.py
+++ Python_Solutions/Project_Euler_Problem_016.py
@@ -1,9 +1,2 @@
-def main():
-    original_num = str(2**1000)
-    sum_of_digits = 0
-
-    for digit in original_num:
-        sum_of_digits += int(digit)
-
-    print(sum_of_digits)
-main()
+# Project Euler Problem 16 - Python 3.4
+power_digit_sum = lambda : sum([int(x) for x in str(2**1000)])
