ee2f7367edf3c05a67bf75ea5df25d4796a95246
Michael Arnold <valestrum@hotmail.com>
2015-12-21 23:19:46 -0500

    Updated Python solution to support Python 3.4

--- Python_Solutions/Project_Euler_Problem_003.py
+++ Python_Solutions/Project_Euler_Problem_003.py
@@ -5,5 +5,5 @@ def main():
         while num % x == 0: 
             num /= x 
         x += 1
-    print num #Prints largest prime of the assigned number (600851475143)
+    print(num) # Prints largest prime of the assigned number (600851475143)
 main()
